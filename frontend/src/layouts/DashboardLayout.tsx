d8b7fcb6849b8321083aed4889b115fd66e4222f
fausto1025 <ingfaustojurado@gmail.com>
2026-01-20 10:46:42 -0600

    cambio 1

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,20 +1,18 @@
 // src/layouts/DashboardLayout.tsx
-import Sidebar from "../components/Sidebar";
+
 import Header from "../components/Header";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="app-layout">
-      {/* Sidebar fijo a la izquierda */}
-      <aside className="app-sidebar">
-        <Sidebar />
-      </aside>
-
-      {/* Columna derecha con header + contenido */}
+     
+     
       <div className="app-main">
         <Header />
         <div className="app-content">{children}</div>
       </div>
+
+      
     </div>
   );
 }
