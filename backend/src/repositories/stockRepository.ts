ad77d19b7ba0d8070dc70b3ff4e36d24b652c081
fausto1025 <ingfaustojurado@gmail.com>
2026-06-16 09:46:32 -0600

    2585

--- backend/src/repositories/stockRepository.ts
+++ backend/src/repositories/stockRepository.ts
@@ -19,7 +19,7 @@ export const stockRepository = {
           "s.servicio as servicio",
           "s.centro_costo as centro_costo",
           "l.id_subalmacen",
-          "l.id_servicio",
+          "sa.id_servicio",
           db.raw("SUM(l.cantidad_actual) as stock"),
           "cs.minimo",
           "cs.maximo"
